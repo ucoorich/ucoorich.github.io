7a112c43380ed98a29d7d284446fc7b16ef00f98
ucoorich <65450185+ucoorich@users.noreply.github.com>
2022-01-29 08:57:20 +0800

    Update index.js

--- racon/EatKano-main/static/index.js
+++ racon/EatKano-main/static/index.js
@@ -441,8 +441,8 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
             SubmitResults();
         }
 
-        if (cps <= 5) return '试着好好练一下？';
-        if (cps <= 8) return 'TCL';
+        if (cps <= 3) return '别玩了菜逼';
+        if (cps <= 5) return '你是浣熊吧';
         if (cps <= 10)  return 'TQL';
         if (cps <= 15) return '您';
         return '人？';
